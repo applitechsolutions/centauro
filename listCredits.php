7dccbdb1b5f995900520d7e5fa8a5936029c4fd8
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-01-28 18:38:26 -0600

    listCredits 1.0

--- listCredits.php
+++ listCredits.php
@@ -25,23 +25,22 @@ include_once 'functions/bd_conexion.php';
                 <div class="col-xl-12">
                     <div class="card mb-3">
                         <div class="card-header">
-                            <h3><i class="fa fa-vcard"></i> Listado general de créditos</h3>
-                            -> Control de clientes disponibles
+                            <h3><i class="fa fa-vcard"></i> Listado general de créditos activos</h3>
+                            -> Aquí puede consultar la infomación de los créditos.
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table id="example1" class="table table-bordered table-hover display">
                                     <thead>
                                         <tr>
-                                            <th>DPI</th>
-                                            <th>Nombre</th>
-                                            <th>Apellido</th>
-                                            <th>Dirección</th>
-                                            <th>Teléfono</th>
-                                            <th>Teléfono 2</th>
+                                            <th>No. de tarjeta</th>
                                             <th>Cobrador</th>
-                                            <th>Ruta</th>
-                                            <th>Negocio</th>
+                                            <th>Cliente</th>
+                                            <th>Dirección</th>
+                                            <th>Comercio</th>
+                                            <th>Teléfono</th>
+                                            <th>Fecha</th>
+                                            <th>Total Q.</th>
                                             <th>Acciones</th>
                                         </tr>
                                     </thead>
@@ -49,9 +48,12 @@ include_once 'functions/bd_conexion.php';
                                         <!-- PHP LiSTADO -->
                                         <?php
 try {
-    $sql = ("SELECT C.*, (select concat(codeRoute,' ', routeName) FROM route WHERE idRoute = C._idRoute) as routeName, (select name FROM commerce WHERE idCommerce = C._idCommerce) as comercio,
-                                        (SELECT concat(firstName,' ',lastName) from collector where idCollector = (select _idCollector from route where idRoute = C._idRoute)) as nombre
-                                        FROM customer C WHERE state = 0;");
+    $sql = ("SELECT C.idCredit, C.code, (select concat(firstName,' ',lastName) from collector where idCollector = C._idCollector) as collector,
+    (select concat(firstName,' ',lastName) from customer where idCustomer = C._idCustomer) as customer,
+    (select address from customer where idCustomer = C._idCustomer) as address,
+    (select name from commerce where idCommerce = (select _idCommerce from customer where idCustomer = C._idCustomer)) as commerce,
+    (select mobile from customer where idCustomer = C._idCustomer) as mobile, C.dateStart, C.total
+    FROM credit C WHERE state = 0 AND cancel = 0;");
 
     $resultado = $conn->query($sql);
 } catch (Exception $e) {
@@ -59,44 +61,50 @@ try {
     echo $error;
 }
 
-while ($customer = $resultado->fetch_assoc()) {
+while ($credit = $resultado->fetch_assoc()) {
+    $fecha = date_create($credit['dateStart']);
     ?>
                                         <!-- PHP LiSTADO -->
                                         <tr>
                                             <td>
-                                                <?php echo $customer['DPI']; ?>
+                                                <H6>
+                                                    <?php echo $credit['code']; ?>
+                                                </H6>
                                             </td>
                                             <td>
-                                                <?php echo $customer['firstName']; ?>
+                                                <STRONg>
+                                                    <?php echo $credit['collector']; ?>
+                                                </STRONg>
                                             </td>
                                             <td>
-                                                <?php echo $customer['lastName']; ?>
+                                                <?php echo $credit['customer']; ?>
                                             </td>
                                             <td>
-                                                <?php echo $customer['address']; ?>
+                                                <?php echo $credit['address']; ?>
                                             </td>
                                             <td>
-                                                <?php echo $customer['mobile']; ?>
+                                                <?php echo $credit['commerce']; ?>
                                             </td>
                                             <td>
-                                                <?php echo $customer['mobile2']; ?>
+                                                <?php echo $credit['mobile']; ?>
                                             </td>
                                             <td>
-                                                <?php echo $customer['nombre']; ?>
+                                                <H6>
+                                                    <?php echo date_format($fecha, 'd/m/Y'); ?>
+                                                </H6>
                                             </td>
                                             <td>
-                                                <?php echo $customer['routeName']; ?>
+                                                <b class="text-success">
+                                                    <?php echo $credit['total']; ?>
+                                                </b>
                                             </td>
                                             <td>
-                                                <?php echo $customer['comercio']; ?>
-                                            </td>
-                                            <td>
-                                                <a href="#" data-id="<?php echo $customer['idCustomer']; ?>" data-tipo="customer"
-                                                    class="btn btn-outline-danger pull-right borrar_cliente" style="
+                                                <a href="#" data-id="<?php echo $credit['idCredit']; ?>" data-tipo="credit"
+                                                    class="btn btn-outline-danger pull-right" style="
 													margin-left: 5px;"><i
                                                         class="fa fa-trash"></i>
                                                     Eliminar</a>
-                                                <a class="btn btn-outline-primary pull-right" href="editCustomer.php?id=<?php echo $customer['idCustomer']; ?>"><i
+                                                <a class="btn btn-outline-primary pull-right" href="editCredit.php?id=<?php echo $credit['idCredit']; ?>"><i
                                                         class="fa fa-pencil"></i> Editar</a>
                                             </td>
                                         </tr>
@@ -119,72 +127,6 @@ while ($customer = $resultado->fetch_assoc()) {
     <!-- END content -->
 </div>
 <!-- END content-page -->
-
-<script>
-//START CODE FOR Child rows (show extra / detailed information) DATA TABLE
-function format(d) {
-    // `d` is the original data object for the row
-    return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">' +
-        '<tr>' +
-        '<td>Full name:</td>' +
-        '<td>' + d.name + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>Extension number:</td>' +
-        '<td>' + d.extn + '</td>' +
-        '</tr>' +
-        '<tr>' +
-        '<td>Extra info:</td>' +
-        '<td>And any further details here (images etc)...</td>' +
-        '</tr>' +
-        '</table>';
-}
-
-$(document).ready(function() {
-    var table = $('#example3').DataTable({
-        "ajax": "assets/data/dataTablesObjects.txt",
-        "columns": [{
-                "className": 'details-control',
-                "orderable": false,
-                "data": null,
-                "defaultContent": ''
-            },
-            {
-                "data": "name"
-            },
-            {
-                "data": "position"
-            },
-            {
-                "data": "office"
-            },
-            {
-                "data": "salary"
-            }
-        ],
-        "order": [
-            [1, 'asc']
-        ]
-    });
-
-    // Add event listener for opening and closing details
-    $('#example2 tbody').on('click', 'td.details-control', function() {
-        var tr = $(this).closest('tr');
-        var row = table.row(tr);
-
-        if (row.child.isShown()) {
-            // This row is already open - close it
-            row.child.hide();
-            tr.removeClass('shown');
-        } else {
-            // Open this row
-            row.child(format(row.data())).show();
-            tr.addClass('shown');
-        }
-    });
-});
-// END CODE FOR Child rows (show extra / detailed information) DATA TABLE
-</script>
 
 <?php
 include_once 'templates/footer.php';
