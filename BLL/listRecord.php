95d0220f975910bc5b469476f8974b98631a87a0
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-02-19 19:32:34 -0600

    DIFF de record

--- BLL/listRecord.php
+++ BLL/listRecord.php
@@ -5,8 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 $idCustomer = $_POST['idCustomer'];
 
 $result = $conn->query("SELECT C.code, C.dateStart, C.total, C.cancel, C.record,
-    (select count(*) from balance where _idCredit = C.idCredit and balpay = 1 and state = 0) as totalP,
-    (select count(*) from balance where _idCredit = C.idCredit and balpay = 1 and state = 0 and amount > 0) as pays
+    datediff((select date from balance where state = 0 ORDER BY idBalance DESC LIMIT 1), C.dateStart) as diff
     FROM credit C WHERE _idCustomer = $idCustomer ORDER BY C.dateStart ASC");
 $outp = array();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
