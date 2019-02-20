22156da2decf298284bdf886f6fccc25e0f55e67
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-02-19 19:34:21 -0600

    Peque;a cagada

--- BLL/listRecord.php
+++ BLL/listRecord.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 $idCustomer = $_POST['idCustomer'];
 
 $result = $conn->query("SELECT C.code, C.dateStart, C.total, C.cancel, C.record,
-    datediff((select date from balance where state = 0 ORDER BY idBalance DESC LIMIT 1), C.dateStart) as diff
+    datediff((select date from balance where _idCredit = C.idCredit state = 0 ORDER BY idBalance DESC LIMIT 1), C.dateStart) as diff
     FROM credit C WHERE _idCustomer = $idCustomer ORDER BY C.dateStart ASC");
 $outp = array();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
