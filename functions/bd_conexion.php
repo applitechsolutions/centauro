c75f90e4a2c2172699b0f6633fdfc0fcca9d4ced
LuisPedroChaves <luispedrochaves@hotmail.com>
2024-07-01 15:25:13 -0600

    Probando nueva conexión

--- functions/bd_conexion.php
+++ functions/bd_conexion.php
@@ -1,8 +1,8 @@
 <?php
 
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/functions/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, "applitech-mysql-flexible.mysql.database.azure.com", "applitech", "28XHc5qt", "centauro", 3306, MYSQLI_CLIENT_SSL);
+// $conn = mysqli_init();
+// mysqli_ssl_set($conn,NULL,NULL, "/home/site/wwwroot/functions/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+// mysqli_real_connect($conn, "applitech-mysql-flexible.mysql.database.azure.com", "applitech", "28XHc5qt", "centauro", 3306, MYSQLI_CLIENT_SSL);
 
 /*$conn = new mysqli('localhost', 'root', '', 'centauro');
 
@@ -10,30 +10,30 @@ if ($conn->connect_error) {
     echo $error -> $conn->connect_error;
 }*/
 
-    // $connectstr_dbhost = '';
-    // $connectstr_dbname = '';
-    // $connectstr_dbusername = '';
-    // $connectstr_dbpassword = '';
+    $connectstr_dbhost = '';
+    $connectstr_dbname = '';
+    $connectstr_dbusername = '';
+    $connectstr_dbpassword = '';
 
-    // foreach ($_SERVER as $key => $value) {
-    //     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-    //         continue;
-    //     }
+    foreach ($_SERVER as $key => $value) {
+        if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+            continue;
+        }
         
-    //     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    //     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    //     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    //     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-    // }
+        $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+        $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    }
 
-    // $conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+    $conn = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
-    // if (!$conn) {
-    //     echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    //     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    //     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    //     exit;
-    // }
+    if (!$conn) {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
 
     /*echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
     echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
